ecd1741569eaa79d5db2013eb709a6ee9e7e49c4
Shrushti-Sakat <shrushtisakat866@gmail.com>
2026-05-26 21:27:40 +0530

    Add redirect configuration for www to non-www

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,16 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   allowedDevOrigins: ['10.63.120.199', '192.168.43.137', '10.139.37.199'],
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [{ type: "host", value: "www.mcadsolution.in" }],
+        destination: "https://mcadsolution.in/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
